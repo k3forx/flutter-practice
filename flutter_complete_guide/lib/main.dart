f3800cde61993828903b18aceebf0d7dd0ed9361
Kanata Miyahana <afiri.bupa@gmail.com>
2022-10-23 22:49:49 +0900

    column

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -7,10 +7,40 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void answerQuestion() {
+    print('Answer Chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('HelloWorld!'),
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            const Text('The question!'),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: const Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: () => {print('Answer 2 chosen!')},
+              child: const Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: () => {print('Answer 3 chosen!')},
+              child: const Text('Answer 3'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
