e30378614b01f5a304ee954c89ef7da1d112eb61
Kanata Miyahana <afiri.bupa@gmail.com>
2022-10-23 22:08:20 +0900

    add @override decorator

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Text('HelloWorld!'),
     );
   }
