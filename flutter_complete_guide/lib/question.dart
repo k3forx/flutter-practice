93c1e4c4725fbb715f0e8606071430577ac2117e
Kanata Miyahana <afiri.bupa@gmail.com>
2022-10-23 23:40:02 +0900

    update design

--- flutter_complete_guide/lib/question.dart
+++ flutter_complete_guide/lib/question.dart
@@ -7,8 +7,9 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       width: double.infinity,
+      margin: const EdgeInsets.all(10),
       child: Text(
         questionText,
         style: const TextStyle(
