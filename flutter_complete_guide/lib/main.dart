7f38c6928f3d485291b3157ad9643dd1d4e0031c
Kanata Miyahana <afiri.bupa@gmail.com>
2022-10-23 23:32:05 +0900

    refactor main.dart

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_complete_guide/question.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -36,7 +38,9 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[_questionIndex]),
+            Question(
+              questions[_questionIndex],
+            ),
             ElevatedButton(
               onPressed: _answerQuestion,
               child: const Text('Answer 1'),
