e7288af39882fa069804f03ab32193331684e18d
Kanata Miyahana <afiri.bupa@gmail.com>
2022-10-26 23:29:09 +0900

    add getter

--- flutter_complete_guide/lib/result.dart
+++ flutter_complete_guide/lib/result.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-  const Result({super.key});
+  final int resultScore;
+
+  const Result(this.resultScore, {super.key});
+
+  String get resultPhrase {
+    var resultText = 'You did it!';
+    if (resultScore <= 8) {
+      resultText = 'You are awesome and innocent!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likable!';
+    } else if (resultScore <= 16) {
+      resultText = 'You are ... strange?!';
+    } else {
+      resultText = 'You are so bad';
+    }
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('You did it!'),
+    return Center(
+      child: Text(
+        resultPhrase,
+        style: const TextStyle(
+          fontSize: 36,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
