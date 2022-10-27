c7f5ef8b6dc9f9639b7c64fbe907ae6ff101cdb9
Kanata Miyahana <afiri.bupa@gmail.com>
2022-10-27 23:38:38 +0900

    more styling

--- personal_expenses/lib/main.dart
+++ personal_expenses/lib/main.dart
@@ -57,18 +57,37 @@ class MyApp extends StatelessWidget {
                         ),
                         decoration: BoxDecoration(
                           border: Border.all(
-                            color: Colors.black,
+                            color: Colors.purple,
                             width: 2,
                           ),
                         ),
                         padding: const EdgeInsets.all(10),
-                        child: Text(tx.amount.toString()),
+                        child: Text(
+                          tx.amount.toString(),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
                       ),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(tx.title),
+                          Text(
+                            tx.title,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
                           Text(
                             tx.date.toString(),
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
                           ),
                         ],
                       )
