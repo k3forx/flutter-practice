402f237577969e0dc6407fef20bdc98b062989a0
Kanata Miyahana <afiri.bupa@gmail.com>
2022-10-27 22:06:30 +0900

    refactor

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/answer.dart';
 
-import 'package:flutter_complete_guide/question.dart';
 import 'package:flutter_complete_guide/quiz.dart';
 import 'package:flutter_complete_guide/result.dart';
 
