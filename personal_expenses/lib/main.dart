6d9d0fdf88c719eeee2eb6085fc1cc83d38ac017
Kanata Miyahana <afiri.bupa@gmail.com>
2022-10-30 00:26:55 +0900

    add comment

--- personal_expenses/lib/main.dart
+++ personal_expenses/lib/main.dart
@@ -101,6 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ],
       ),
+      // キーボードを表示した時にエラーにならないためのSingleChildScrollView
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
