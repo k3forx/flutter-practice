4e0b66c587e45ca018de34d8af9c0ddee389e9dc
Kanata Miyahana <afiri.bupa@gmail.com>
2022-10-29 22:33:06 +0900

    add chart.dart

---
+++ personal_expenses/lib/widgets/chart.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:personal_expenses/models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  const Chart({super.key, required this.recentTransactions});
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+
+      double totalSum = 0;
+      for (var i = 0; i < recentTransactions.length; i++) {
+        Transaction tx = recentTransactions[i];
+        if (tx.date.day == weekDay.day &&
+            tx.date.month == weekDay.month &&
+            tx.date.year == weekDay.year) {
+          totalSum += tx.amount;
+        }
+      }
+
+      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: const EdgeInsets.all(20),
+      child: Row(
+        children: [
+          Column(),
+        ],
+      ),
+    );
+  }
+}
--- personal_expenses/lib/widgets/transaction_list.dart
+++ personal_expenses/lib/widgets/transaction_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
 import 'package:personal_expenses/models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
