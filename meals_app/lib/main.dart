131ea3dd5b1fa15203e68fc3c0e0a101eb9aa831
Kanata Miyahana <afiri.bupa@gmail.com>
2022-11-04 18:03:06 +0900

    add category class

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -10,19 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'DeliMeals',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
