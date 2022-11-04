bef6d2f0320729112629a113e98a58808e57232c
Kanata Miyahana <afiri.bupa@gmail.com>
2022-11-04 18:22:54 +0900

    show categories screen

--- meals_app/lib/categories_screen.dart
+++ meals_app/lib/categories_screen.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'package:meals_app/category_item.dart';
+import 'package:meals_app/dummy_data.dart';
+
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: [],
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisExtent: 20,
+    return Scaffold(
+      appBar: AppBar(title: const Text('DeliMeal')),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((category) =>
+                CategoryItem(title: category.title, color: category.color))
+            .toList(),
       ),
     );
   }
--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:meals_app/categories_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,28 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeals'),
-      ),
-      body: const Center(
-        child: Text('Navigation Time!'),
-      ),
+      home: CategoriesScreen(),
     );
   }
 }
