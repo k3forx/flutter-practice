45b75b3b6819c94c978efb39274db8ece74b001e
Kanata Miyahana <afiri.bupa@gmail.com>
2022-11-04 22:31:35 +0900

    mkdir

--- meals_app/lib/screens/category_meals_screen.dart
+++ meals_app/lib/screens/category_meals_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:meals_app/dummy_data.dart';
+import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/models/meal.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
