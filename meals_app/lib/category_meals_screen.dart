f9cdb80f1ee626dc0980d011a105b7f20c10b085
Kanata Miyahana <afiri.bupa@gmail.com>
2022-11-04 22:28:58 +0900

    show meals

--- meals_app/lib/category_meals_screen.dart
+++ meals_app/lib/category_meals_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:meals_app/dummy_data.dart';
+import 'package:meals_app/models/meal.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
   // final String categoryId;
@@ -16,9 +19,21 @@ class CategoryMealsScreen extends StatelessWidget {
     final routeArgs =
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle!),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          final Meal meal = categoryMeals[index];
+          return Text(meal.title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
