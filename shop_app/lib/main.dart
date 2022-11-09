4dacf3e142ad8efff136bca65826dfddab47cf6d
Kanata Miyahana <afiri.bupa@gmail.com>
2022-11-09 23:28:01 +0900

    rename

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/screents/products_overview_screen.dart';
+
+import 'package:shop_app/screens/products_overview_screen.dart';
 
 void main() {
   runApp(const MyApp());
